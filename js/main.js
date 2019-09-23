7d2da4db7a2ebe038bd809614b7318425f88a234
symon3000 <maxcooper@Courtneys-Air.PK5001Z>
2019-09-23 17:20:42 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -162,3 +162,18 @@ $(window).load(function(){
   });
 
 });
+
+
+
+// Mobile hover effect (click)
+
+var mobileHover = function () {
+    $('*').on('touchstart', function () {
+        $(this).trigger('hover');
+    }).on('touchend', function () {
+        $(this).trigger('hover');
+    });
+};
+
+mobileHover();
+
